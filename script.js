d03c9d5e4c6ed0f6dd5f9bc3cc9276c87059e4b5
Gabor Yaxa <gabor.flandorffer@yaxa.co>
2018-10-07 22:36:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 var new_div = document.createElement("div");
 new_div.innerHTML = "<img src='https://skinmce.yaxa.co/frontend/accessshop/default/images/logo-yaxa.png'>";
 document.body.insertBefore(new_div, document.body.firstChild);
-
-document.head.appendChild(new_div);
