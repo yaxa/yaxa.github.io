96c4b1f6a96f999e1a9ef04ac0ae0d9a8618f601
Gabor Yaxa <gabor.flandorffer@yaxa.co>
2018-10-07 21:15:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("xxy");
+var new_div = document.createElement("div");
+new_div.innerHTML = "<img src='https://skinmce.yaxa.co/frontend/accessshop/default/images/logo-yaxa.png'>";
+document.body.insertBefore(new_div, document.body.firstChild);
+
+document.head.appendChild(new_div);
